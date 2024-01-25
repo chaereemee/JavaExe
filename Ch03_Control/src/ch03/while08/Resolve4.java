8bc577bbc02fefc7575d15ed7bc84a8752c9d56b
Lee, Chaerim <leechaerim214@gmail.com>
2024-01-25 16:36:59 +0900

    문제 풀기

---
+++ Ch03_Control/src/ch03/while08/Resolve4.java
@@ -0,0 +1,18 @@
+package ch03.while08;
+
+public class Resolve4 {
+
+	public static void main(String[] args) {
+		int num = 1;
+		while(num <= 10) {
+			if(num % 2 == 0) {
+				System.out.print(num + ", ");
+				num++;
+			}
+			else {
+				num++;
+			}
+		}
+	}
+
+}
