e9a1bb1bd7fbf0468f64de20e72f392ed7ac91ee
Lee, Chaerim <leechaerim214@gmail.com>
2024-01-26 15:22:34 +0900

    Array.toString()

---
+++ Ch05_Array/src/ch05/array04/ArrayEx.java
@@ -0,0 +1,18 @@
+package ch05.array04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayEx {
+
+	public static void main(String[] args) {
+		String[] names = {"홍길동", "임꺽정", "차돌바위"};
+		
+		System.out.println(Arrays.toString(names));
+		for(int i=0; i<names.length; i++) {
+			System.out.print(names[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
