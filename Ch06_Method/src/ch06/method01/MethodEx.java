6c9522fca00277f0dff115dda12755f540d32454
Lee, Chaerim <leechaerim214@gmail.com>
2024-01-29 14:47:18 +0900

    메소드 만들기 전

--- Ch06_Method/src/ch06/method01/MethodEx.java
+++ Ch06_Method/src/ch06/method01/MethodEx.java
@@ -1,10 +1,45 @@
 package ch06.method01;
 
+import java.util.Scanner;
+
+// 메서드를 만들기 전 코드 
 public class MethodEx {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 
+		System.out.print("1번째 점수 입력 >> ");
+		int num0 = sc.nextInt();
+		System.out.print("2번째 점수 입력 >> ");
+		int num1 = sc.nextInt();
+		System.out.print("연산자 입력(+ - * / %) >> ");
+		String op = sc.next();
+		
+		int result = 0;
+		
+		switch(op) {
+		case "+":
+			result = num0 + num1;
+			break;
+		case "-":
+			result = num0 - num1;
+			break;
+		case "*":
+			result = num0 * num1;
+			break;
+		case "/":
+			result = num0 / num1;
+			break;
+		case "%":
+			result = num0 % num1;
+			break;
+		default:
+			System.out.println("연산이 불가능합니다.");
+			break;
+		}
+		System.out.println("결과값은 " + result + "입니다.");
+		
+		sc.close();
 	}
 
 }
