9fabe54f258f9918012e8f5db495fa83582426ac
Lee, Chaerim <leechaerim214@gmail.com>
2024-01-31 16:19:58 +0900

    extends1

--- Ch09_Extends/src/ch09/extends01/Child.java
+++ Ch09_Extends/src/ch09/extends01/Child.java
@@ -1,10 +1,7 @@
 package ch09.extends01;
 
-public class Child extends Father {
-	
-	float day = 365 + 1.0f/4;
-	
+public class Child extends Father{
 	void play() {
-		System.out.println("인생이 즐겂다.");
+		System.out.println("인생이 즐겁다^^");
 	}
 }
--- Ch09_Extends/src/ch09/extends01/GrandFather.java
+++ Ch09_Extends/src/ch09/extends01/GrandFather.java
@@ -1,10 +1,7 @@
 package ch09.extends01;
 
 public class GrandFather {
-	
-	int handsomeScore = 10;
-	
 	void handsome() {
-		System.out.println("잘생겼다.");
+		System.out.println("잘 생겼다~");
 	}
 }
