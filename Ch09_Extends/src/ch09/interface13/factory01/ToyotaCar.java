b065c1986525ee21e96c4713d2b31c36af8fa466
Lee, Chaerim <leechaerim214@gmail.com>
2024-02-02 15:30:01 +0900

    인터페이스

---
+++ Ch09_Extends/src/ch09/interface13/factory01/ToyotaCar.java
@@ -0,0 +1,31 @@
+package ch09.interface13.factory01;
+
+import ch09.interface13.ICar;
+
+public class ToyotaCar implements ICar {
+
+	@Override
+	public void turnOn() throws InterruptedException {
+		System.out.println("ToyotaCar - 시동이 부드럽게 잘 걸린다~T");
+		Thread.sleep((long)Math.random()*1000);
+	}
+
+	@Override
+	public void turnOff() throws InterruptedException {
+		System.out.println("ToyotaCar - 시동이 부드럽게 잘 Off~T");
+		Thread.sleep((long)Math.random()*1000);
+	}
+
+	@Override
+	public void accel() throws InterruptedException {
+		System.out.println("ToyotaCar - 액셀이 터보로 동작된다~T");
+		Thread.sleep((long)Math.random()*1000);
+	}
+
+	@Override
+	public void footBreak() throws InterruptedException {
+		System.out.println("ToyotaCar - 브레이크가 부드럽게 동작된다~T");
+		Thread.sleep((long)Math.random()*1000);
+	}
+
+}
