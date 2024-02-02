4c575eb91a7235d794ca6d5d1f027a1c3158d42f
Lee, Chaerim <leechaerim214@gmail.com>
2024-02-02 16:45:17 +0900

    필드 또한 상속이 된다.

---
+++ Ch09_Extends/src/ch09/extends02/Father.java
@@ -0,0 +1,10 @@
+package ch09.extends02;
+
+public class Father extends GrandFather {
+	
+	long money = 10000000000000000L;
+	
+	void wealth() {    // wealth : 재산 
+		System.out.println("돈을 많이 벌었다ㅎ");
+	}
+}
