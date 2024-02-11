093ce1c03e6dcc4d85506da2f7b3c1d000e6e115
Lee, Chaerim <leechaerim214@gmail.com>
2024-02-12 00:05:19 +0900

    클래스를 제네릭 타입으로

--- Ch13_Generic/src/ch13/sec02/exam01/GenericExample.java
+++ Ch13_Generic/src/ch13/sec02/exam01/GenericExample.java
@@ -17,12 +17,12 @@ public class GenericExample {
 		// K는 Car로 대체, M은 String으로 대체
 		Product<Car, String> product2 = new Product<>();
 
-		// Setter 매개값은 반드시 Tv와 String을 제공
+		// Setter 매개값은 반드시 Car와 String을 제공
 		product2.setKind(new Car());
 		product2.setModel("SUV자동차");
 
-		// Getter 리턴값은 Tv와 String이 됨
-		Tv Car = product1.getKind();
+		// Getter 리턴값은 Car와 String이 됨
+		Car car = product2.getKind();
 		String carModel = product2.getModel();
 	}
 
