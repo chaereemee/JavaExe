42bb3cecaf7fee874cc3b3ca8757343ed665562e
Lee, Chaerim <leechaerim214@gmail.com>
2024-02-13 10:30:40 +0900

    동시 작업 -> 작업스레드 사용

---
+++ Ch15_Thread/src/ch15/thread01/ch14/sec03/exam02/BeepPrintExample.java
@@ -0,0 +1,35 @@
+package ch15.thread01.ch14.sec03.exam02;
+
+import java.awt.Toolkit;
+
+public class BeepPrintExample {
+	public static void main(String[] args) {
+		// 작업 스레드 생성 
+		Thread thread = new Thread(new Runnable() {
+//			@Override
+			public void run() {
+				// 작업 스레드가 실행하는 코드 
+				Toolkit toolkit = Toolkit.getDefaultToolkit(); 
+				for (int i = 0; i < 5; i++) {
+					toolkit.beep();
+					try {
+						Thread.sleep(500);
+					} catch (Exception e) {
+					}
+				}
+			}
+		});
+		
+		// 작업 스레드 실행 
+		thread.start();
+		
+		// 메인 스레드가 실행하는 코드 
+		for(int i=0; i<5; i++) {
+			System.out.println("띵");
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+			}
+		}
+	}
+}
