b10d6e72f8983fc324462e8cdaec448978bb6505
Lee, Chaerim <leechaerim214@gmail.com>
2024-02-14 10:48:46 +0900

    NullPointerException

--- Ch14_Exception/src/ch14/exception01/ch11/sec02/exam01/ExceptionHandlingExample1.java
+++ Ch14_Exception/src/ch14/exception01/ch11/sec02/exam01/ExceptionHandlingExample1.java
@@ -1,15 +1,15 @@
 package ch14.exception01.ch11.sec02.exam01;
 
-public class ExceptionHHandlingExample {
+public class ExceptionHandlingExample1 {
 	public static void printLength(String data) {
-		int result = data.length();
+		int result = data.length();   // 문자열이 null일 경우 NullPointerException 발생 
 		System.out.println("문자 수: " + result);
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("[프로그램 시작]\n");
 		printLength("ThisIsJava");
-		printLength(null);
+		printLength(null);            // 매개값으로 null 대입 
 		System.out.println("[프로그램 종료]");
 	}
 }
